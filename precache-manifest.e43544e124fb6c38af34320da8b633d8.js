d1f142fa32c9334e577c88aec638b437d9569980
praveshstark <2018510091@iiitvadodara.ac.in>
2020-10-14 22:41:02 +0530

    Updates

---
+++ precache-manifest.e43544e124fb6c38af34320da8b633d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a711015f48cc4c013e3b351922c6a4",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "6096f54809f30d1327ae",
+    "url": "/reactlivewebsite/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "be4f799ba4d496035a7e",
+    "url": "/reactlivewebsite/static/css/main.ebc9e823.chunk.css"
+  },
+  {
+    "revision": "6096f54809f30d1327ae",
+    "url": "/reactlivewebsite/static/js/2.2e147261.chunk.js"
+  },
+  {
+    "revision": "706ae2ba09b6b4555454ecd6058d6910",
+    "url": "/reactlivewebsite/static/js/2.2e147261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4f799ba4d496035a7e",
+    "url": "/reactlivewebsite/static/js/main.f41cb1ce.chunk.js"
+  },
+  {
+    "revision": "fb68cd84534c5e71e2ee",
+    "url": "/reactlivewebsite/static/js/runtime-main.6ae36270.js"
+  },
+  {
+    "revision": "14ad6ecfc89fa85f739ec73d5ba25c3e",
+    "url": "/reactlivewebsite/static/media/img1.14ad6ecf.jpg"
+  },
+  {
+    "revision": "c95400422d618f92262d9519eb3704cf",
+    "url": "/reactlivewebsite/static/media/img2.c9540042.jpeg"
+  },
+  {
+    "revision": "9d14411e645743664282a11dc2f7197c",
+    "url": "/reactlivewebsite/static/media/img3.9d14411e.jpeg"
+  }
+]);
